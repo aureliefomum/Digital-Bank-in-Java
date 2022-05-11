6d23a8856c7917927bbd79f3bbc43e409181fd38
aureliefomum <azam_aurelie@yahoo.com>
2022-05-11 14:19:02 -0300

    added method imprimirListaDeClientes to class Banco

--- src/main/java/bancoDigital/Banco.java
+++ src/main/java/bancoDigital/Banco.java
@@ -1,13 +1,38 @@
 package bancoDigital;
 
-public class Banco {
-    public String nome;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Banco {
+    private String nome;
+    private List<Conta> contas = new ArrayList<>();
+
+
+
+    public void adicionaNovaConta(Conta novaConta) {
+       contas.add(novaConta) ;
+   }
+
+   public void imprimirListaClientes() {
+       System.out.println("=== Lista de Clientes do Banco ===");
+       for (Conta conta : contas) {
+
+           System.out.println("Nome do cliente:  " + conta.cliente.getNome()+ ": " + conta.getClass().getSimpleName());
+       }
+   }
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
     }
 }
